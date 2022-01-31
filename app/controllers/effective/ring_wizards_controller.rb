93742cfb9ec795f3f1d14e0e2a9f62a53036a8f6
Matt Riemer <matthew@codeandeffect.com>
2022-01-31 10:32:19 -0700

    work with newer effective resources

--- app/controllers/effective/ring_wizards_controller.rb
+++ app/controllers/effective/ring_wizards_controller.rb
@@ -6,26 +6,5 @@ module Effective
 
     resource_scope -> { EffectiveProducts.RingWizard.deep.where(owner: current_user) }
 
-    # Allow only 1 in-progress application at a time
-    before_action(only: [:new, :show], unless: -> { resource&.done? }) do
-      existing = resource_scope.in_progress.where.not(id: resource).first
-
-      if existing.present?
-        flash[:success] = "You have been redirected to your existing in progress payment"
-        redirect_to effective_products.ring_wizard_build_path(existing, existing.next_step)
-      end
-    end
-
-    after_save do
-      flash.now[:success] = ''
-    end
-
-    private
-
-    def permitted_params
-      model = (params.key?(:effective_ring_wizard) ? :effective_ring_wizard : :ring_wizard)
-      params.require(model).permit!.except(:status, :status_steps, :wizard_steps, :submitted_at)
-    end
-
   end
 end
