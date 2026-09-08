5092ebf19774d2e8eaf6a7dc15ad5f074128dd7d
Matt Riemer <matthew@codeandeffect.com>
2026-09-08 16:15:54 -0600

    Turbo

--- app/datatables/effective_ring_wizards_datatable.rb
+++ app/datatables/effective_ring_wizards_datatable.rb
@@ -20,7 +20,7 @@ class EffectiveRingWizardsDatatable < Effective::Datatable
 
     actions_col(actions: []) do |resource|
       if resource.draft?
-        dropdown_link_to('Continue', effective_products.ring_wizard_build_path(resource, resource.next_step), 'data-turbolinks' => false)
+        dropdown_link_to('Continue', effective_products.ring_wizard_build_path(resource, resource.next_step), 'data-turbolinks' => false, 'data-turbo' => false)
         dropdown_link_to('Delete', effective_products.ring_wizard_path(resource), 'data-confirm': "Really delete #{resource}?", 'data-method': :delete)
       else
         dropdown_link_to('Show', effective_products.ring_wizard_path(resource))
--- app/datatables/effective_stamp_wizards_datatable.rb
+++ app/datatables/effective_stamp_wizards_datatable.rb
@@ -20,7 +20,7 @@ class EffectiveStampWizardsDatatable < Effective::Datatable
 
     actions_col(actions: []) do |resource|
       if resource.draft?
-        dropdown_link_to('Continue', effective_products.stamp_wizard_build_path(resource, resource.next_step), 'data-turbolinks' => false)
+        dropdown_link_to('Continue', effective_products.stamp_wizard_build_path(resource, resource.next_step), 'data-turbolinks' => false, 'data-turbo' => false)
         dropdown_link_to('Delete', effective_products.stamp_wizard_path(resource), 'data-confirm': "Really delete #{resource}?", 'data-method': :delete)
       else
         dropdown_link_to('Show', effective_products.stamp_wizard_path(resource))
